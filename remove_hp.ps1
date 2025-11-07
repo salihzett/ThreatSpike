3c57089a27d7efa7b2e6476146d295f2d33710d8
salihzett <salihzett@users.noreply.github.com>
2025-11-07 21:34:52 +0100

    Update remove_hp.ps1

--- remove_hp.ps1
+++ remove_hp.ps1
@@ -80,7 +80,7 @@ $uninstallRoots = @(
 foreach ($root in $uninstallRoots) {
     Get-ChildItem $root | ForEach-Object {
         $p = Get-ItemProperty $_.PSPath -ErrorAction SilentlyContinue
-        if ($p.DisplayName -match '(?i)HP|Wolf|Bromium|Security Update') {
+        if ($p.DisplayName -match '(?i)HP|Wolf|Bromium') {
             Write-Host "→ Deinstalliere: $($p.DisplayName)" -ForegroundColor Yellow
             $cmd = $p.UninstallString
             $success = $false
