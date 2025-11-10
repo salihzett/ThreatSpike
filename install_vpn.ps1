18a4f886949b5c2af8c14d9be9e34ea77c9d2ae1
salihzett <salihzett@users.noreply.github.com>
2025-11-10 20:34:34 +0100

    Update install_vpn.ps1

--- install_vpn.ps1
+++ install_vpn.ps1
@@ -1,29 +1,5 @@
-$Log = "C:\Temp\WG-MVPN-SSL-install.log"
-$Installer = "C:\Temp\WG-MVPN-SSL_12_11_4.exe"
-$Url = "https://cdn.watchguard.com/SoftwareCenter/Files/MUVPN_SSL/12_11_4/WG-MVPN-SSL_12_11_4.exe"
+curl -o C:\Temp\WG-MVPN-SSL.exe https://cdn.watchguard.com/SoftwareCenter/Files/MUVPN_SSL/12_11_4/WG-MVPN-SSL_12_11_4.exe
 
-"$(Get-Date) - Start Download..." | Out-File $Log -Append
+C:\Temp\WG-MVPN-SSL.exe /S
 
-try {
-    Invoke-WebRequest -Uri $Url -OutFile $Installer -ErrorAction Stop
-    "$(Get-Date) - Download done." | Out-File $Log -Append
-}
-catch {
-    "$(Get-Date) - Download error: $_" | Out-File $Log -Append
-    exit 1
-}
-
-"$(Get-Date) - Start Silent Installation..." | Out-File $Log -Append
-try {
-    $proc = Start-Process -FilePath $Installer -ArgumentList "/S" -Wait -PassThru
-    "$(Get-Date) - Installation done. ExitCode: $($proc.ExitCode)" | Out-File $Log -Append
-}
-catch {
-    "$(Get-Date) - Error during Installation: $_" | Out-File $Log -Append
-    exit 1
-}
-
-"$(Get-Date) - Delete Installer..." | Out-File $Log -Append
-Remove-Item $Installer -Force
-
-"$(Get-Date) - Done." | Out-File $Log -Append
+del C:\Temp\WG-MVPN-SSL.exe
