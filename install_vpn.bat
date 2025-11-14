999d7f980da06d2c84e20f42fc86355ba189c1cc
salihzett <salihzett@users.noreply.github.com>
2025-11-14 23:43:58 +0100

    Delete install_vpn.bat

--- install_vpn.bat
+++
@@ -1,5 +0,0 @@
-curl -o C:\Temp\WG-MVPN-SSL.exe https://cdn.watchguard.com/SoftwareCenter/Files/MUVPN_SSL/12_11_4/WG-MVPN-SSL_12_11_4.exe
-
-C:\Temp\WG-MVPN-SSL.exe /S
-
-del C:\Temp\WG-MVPN-SSL.exe
